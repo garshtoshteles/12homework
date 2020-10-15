12fb9cf2486609f44bb8bbb0b273573fd3f0f2c2
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-10-15 18:39:15 -0400

    fixed scope issue with (this) and now everything works as it should. to deploy!

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,16 +2,16 @@ $(() => {
   $(".create-burger").on("submit", (a) => {
     a.preventDefault();
     const b = { name: $("#burgername").val().trim() };
-    console.log("New burger going out ", b),
+    console.log("New burger going out ", b.name),
       $.ajax("/api/burgers", { type: "POST", data: b }).then(() => {
         console.log("new burger made"), location.reload();
       });
   }),
-    $(".devour").on("click", () => {
+    $(".devour").on("click", function () {
       const a = $(this).data("id"),
         b = { devoured: !0 };
       console.log("burgerID sent: ", a),
-        console.log("devoured is: ", b),
+        console.log("devoured is: ", b.devoured),
         $.ajax("api/burgers/" + a, { type: "PUT", data: b }).then(() => {
           console.log("Devoured value: true"), location.reload();
         });
